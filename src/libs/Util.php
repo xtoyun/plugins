23903c0b31a4f686a7d1b6370ae349825975fbe6
xtoyun <yun@xtocn.com>
2019-01-04 11:40:25 +0800

    'plugins'

--- src/libs/Util.php
+++ src/libs/Util.php
@@ -1,11 +1,8 @@
 <?
 /**
  * ============================================================================
- * * 版权所有 2013-2017 xtoyun.net，并保留所有权利。
+ * * 版权所有 2013-2018 xtoyun.net，并保留所有权利。
  * 网站地址: http://www.xtoyun.net；
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和
- * 使用；不允许对程序代码以任何形式任何目的的再发布。
  * ============================================================================
  * $Author: xtoyun $ 
 */
@@ -95,25 +92,4 @@ class Util{
             return $xml;
         }
     }
-
-	static function gourl($url,$paras){
-		$url=url($url).'?'.$paras;
-		header("Location:$url");
-		return;
-	}
-
-	static function getdate(){
-		return date("Y-m-d H:i:s");
-	}
-
-	static function str_rand($length = 32, $char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
-		if(!is_int($length) || $length < 0) {
-		 return false;
-		}
-		$string = '';
-		for($i = $length; $i > 0; $i--) {
-		 $string .= $char[mt_rand(0, strlen($char) - 1)];
-		} 
-		return $string; 
- 	}
 }
